e36ef0b86a93cac114702726173d052ec709700d
kaly-77 <kaly.salas.7@gmail.com>
2023-05-18 08:30:35 +0100

    Initial commit: chap3-Docker

---
+++ src/main/java/com/kaly7dev/notificationservice/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.kaly7dev.notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+
+}
---
+++ src/test/java/com/kaly7dev/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaly7dev.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
